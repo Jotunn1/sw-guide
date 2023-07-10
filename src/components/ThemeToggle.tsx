6d57d02c28ca62d38e4bf5216712e79fc1a2b2d4
Ihor Bozhuk <88133512+Jotunn1@users.noreply.github.com>
2023-07-10 11:06:50 +0300

    fixed initial theme issue

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,31 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
-import { changeToDark, changeToWhite } from "../features/theme/ThemeSlice";
+import {
+  changeToDark,
+  changeToWhite,
+  selectTheme,
+} from "../features/theme/ThemeSlice";
+import { useSelector } from "react-redux";
 
 const ThemeToggle = () => {
-    const [isToggled, setIsToggled] = useState(false);
-    const dispatch = useDispatch();
+  const selectedTheme = useSelector(selectTheme);
+  const [isToggled, setIsToggled] = useState(selectedTheme === "white");
+  const dispatch = useDispatch();
 
-    const toggleClickHandler = () => {
-        setIsToggled(!isToggled);
-        isToggled ? dispatch(changeToDark()) : dispatch(changeToWhite());
-    };
+  const toggleClickHandler = () => {
+    setIsToggled(!isToggled);
+    isToggled ? dispatch(changeToDark()) : dispatch(changeToWhite());
+  };
 
-    return (
-        <div className="wrapper">
-            <div
-                className={`switch ${isToggled ? "left" : "right"}`}
-                onClick={toggleClickHandler}
-            >
-                <div className="bb8-head"></div>
-                <div className="bb8-body">
-                    <div className="circle1"></div>
-                    <div className="circle2"></div>
-                    <div className="circle3"></div>
-                </div>
-            </div>
+  return (
+    <div className="wrapper">
+      <div
+        className={`switch ${isToggled ? "left" : "right"}`}
+        onClick={toggleClickHandler}
+      >
+        <div className="bb8-head"></div>
+        <div className="bb8-body">
+          <div className="circle1"></div>
+          <div className="circle2"></div>
+          <div className="circle3"></div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ThemeToggle;
--- src/features/theme/ThemeSlice.ts
+++ src/features/theme/ThemeSlice.ts
@@ -1,21 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const themeSlice = createSlice({
-    name: "theme",
-    initialState: {
-        currentTheme: "dark",
+  name: "theme",
+  initialState: {
+    currentTheme: "dark",
+  },
+  reducers: {
+    changeToWhite: (state) => {
+      state.currentTheme = "white";
     },
-    reducers: {
-        changeToWhite: (state) => {
-            state.currentTheme = "white";
-        },
-        changeToDark: (state) => {
-            state.currentTheme = "dark";
-        },
+    changeToDark: (state) => {
+      state.currentTheme = "dark";
     },
+  },
 });
 
 export const { changeToWhite, changeToDark } = themeSlice.actions;
 export const selectTheme = (state: { theme: { currentTheme: any } }) =>
-    state.theme.currentTheme;
+  state.theme.currentTheme;
 export default themeSlice.reducer;
