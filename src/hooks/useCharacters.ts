2e7ed44d7b9a5584d6e110aed31ae92ffc27033d
Ihor Bozhuk <88133512+Jotunn1@users.noreply.github.com>
2023-07-10 12:58:31 +0300

    created useCharacters hook with infinite query usage

---
+++ src/hooks/useCharacters.ts
@@ -0,0 +1,43 @@
+import { useMemo } from "react";
+import { apiURL } from "../api";
+import { useInfiniteQuery } from "@tanstack/react-query";
+
+export const useCharacters = () => {
+  const { data, error, fetchNextPage, status, hasNextPage } = useInfiniteQuery(
+    ["characters"],
+    ({ pageParam = 1 }) =>
+      fetch(`${apiURL}people/?page=${pageParam}`).then((res) => res.json()),
+    {
+      getNextPageParam: (lastPage) => {
+        // console.log(lastPage, "lastPage");
+        const previousPage = lastPage.previous
+          ? +lastPage.previous.split("=")[1]
+          : 0;
+        const currentPage = previousPage + 1;
+
+        if (currentPage === lastPage.pages) return false;
+        return currentPage + 1;
+      },
+    }
+  );
+
+  const characters = useMemo(
+    () =>
+      data?.pages.reduce((prev, page) => {
+        return {
+          info: page.info,
+          results: [...prev.results, ...page.results],
+        };
+      }),
+    [data]
+  );
+
+  return {
+    error,
+    fetchNextPage,
+    status,
+    hasNextPage,
+    characters,
+    data,
+  };
+};
--- src/pages/CharactersListPage.tsx
+++ src/pages/CharactersListPage.tsx
@@ -1,9 +1,30 @@
+import { useEffect } from "react";
+import { useCharacters } from "../hooks/useCharacters";
+// import InfiniteScroll from "react-infinite-scroll-component";
+import Preloader from "../components/Preloader";
+
 const CharactersListPage = () => {
-    return (
-        <>
-            <h1 className="page-title">Characters</h1>
-        </>
-    );
+  const { characters, error, fetchNextPage, hasNextPage, status } =
+    useCharacters();
+  useEffect(() => console.log(characters), [characters]);
+  const isLoading = status === "loading";
+  
+  if (status === "error") return <h4>oops!, {`${error}`}</h4>;
+
+  return (
+    <>
+      <h1 className="page-title">Characters</h1>
+      {isLoading && <Preloader />}
+      {!!characters &&
+        characters.results.map((el: any) => <div key={el.name}>{el.name}</div>)}
+      <button
+        onClick={() => fetchNextPage()}
+        // disabled={!data.pageParams.length}
+      >
+        Load More
+      </button>
+    </>
+  );
 };
 
 export default CharactersListPage;
