be7a6b1eb63c976f2ebc94b550db39a4e9429f8a
Ihor Bozhuk <88133512+Jotunn1@users.noreply.github.com>
2023-11-16 21:09:19 +0200

    created list view pages for species & starships

--- src/pages/SpeciesListPage.tsx
+++ src/pages/SpeciesListPage.tsx
@@ -1,9 +1,37 @@
+import InfiniteScroll from "react-infinite-scroll-component";
+import Preloader from "../components/Preloader";
+import { useSpecies } from "../hooks/useSpecies";
+import SpecieCard from "../components/cards/SpecieCard";
+
 const SpeciesListPage = () => {
-    return (
-        <>
-            <h1 className="page-title">Species</h1>
-        </>
-    );
+  const { species, error, fetchNextPage, hasNextPage, status } = useSpecies();
+
+  const isLoading = status === "loading";
+  if (status === "error") return <h4>oops!, {`${error}`}</h4>;
+
+  return (
+    <>
+      <h1 className="page-title">Species</h1>
+      {isLoading ? (
+        <Preloader />
+      ) : (
+        <InfiniteScroll
+          dataLength={species ? species.results.length : 0}
+          next={() => fetchNextPage()}
+          hasMore={!!hasNextPage}
+          loader={<p className="load-text">Loading more ...</p>}
+          style={{ overflow: "visible" }}
+        >
+          <ul className="cards-list species">
+            {!!species &&
+              species.results.map((el: any, index: number) => (
+                <SpecieCard key={index} name={el.name} id={index + 1} />
+              ))}
+          </ul>
+        </InfiniteScroll>
+      )}
+    </>
+  );
 };
 
 export default SpeciesListPage;
--- src/pages/StarshipsListPage.tsx
+++ src/pages/StarshipsListPage.tsx
@@ -1,9 +1,38 @@
+import InfiniteScroll from "react-infinite-scroll-component";
+import Preloader from "../components/Preloader";
+import StarshipCard from "../components/cards/StarshipCard";
+import { useStarships } from "../hooks/useStarships";
+
 const StarshipsListPage = () => {
-    return (
-        <>
-            <h1 className="page-title">Starships</h1>
-        </>
-    );
+  const { starships, error, fetchNextPage, hasNextPage, status } =
+    useStarships();
+
+  const isLoading = status === "loading";
+  if (status === "error") return <h4>oops!, {`${error}`}</h4>;
+
+  return (
+    <>
+      <h1 className="page-title">starships</h1>
+      {isLoading ? (
+        <Preloader />
+      ) : (
+        <InfiniteScroll
+          dataLength={starships ? starships.results.length : 0}
+          next={() => fetchNextPage()}
+          hasMore={!!hasNextPage}
+          loader={<p className="load-text">Loading more ...</p>}
+          style={{ overflow: "visible" }}
+        >
+          <ul className="cards-list starships">
+            {!!starships &&
+              starships.results.map((el: any, index: number) => (
+                <StarshipCard key={index} name={el.name} id={index + 1} />
+              ))}
+          </ul>
+        </InfiniteScroll>
+      )}
+    </>
+  );
 };
 
 export default StarshipsListPage;
